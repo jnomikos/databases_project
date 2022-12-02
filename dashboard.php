349fe83635b7c3112689c34f4146da768085285c
jnomikos <john.nomikos55@gmail.com>
2022-12-01 20:32:37 -0600

    Added navbar items to owner dashboard

--- dashboard.php
+++ dashboard.php
@@ -32,12 +32,22 @@
 	          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="products.php">Products</a>
+          </li>
+      </ul>
       
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="logout.php">Logout</a>
           </li>
+        </ul>
       </div>
     </div>
   </nav>
--- inventory.php
+++ inventory.php
@@ -32,6 +32,15 @@
 	          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="emporders.php">Orders</a>
+          </li>
+      </ul>
       
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
